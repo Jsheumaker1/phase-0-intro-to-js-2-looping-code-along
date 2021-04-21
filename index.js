6e0b8258cccc59fa1c01285a8a05367420084abc
Jsheumaker1 <Jsheumaker1@gmail.com>
2021-04-21 12:48:37 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(cards, event){
+    const newCards=[]
+        for (let i = 0; i < cards.length; i++){
+            newCards.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`);
+        }
+    
+    return newCards;
+}
+
+writeCards(["Lisa", "Kaitlin", "Jan"], surprise)
+
+function countDown(integer){
+    while (integer>=0){
+        console.log(integer);
+        integer--;
+    }
+}
+
+countDown (6);
